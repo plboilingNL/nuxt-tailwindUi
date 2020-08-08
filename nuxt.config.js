135b2fe375c532613f42bc4f35d6906d7b6be6f7
plboilingNL <mwlodarski@icloud.com>
2020-08-08 20:20:38 +0200

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,14 @@
+export default {
+  /*
+   ** Nuxt.js modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    "@nuxtjs/tailwindcss",
+    // Doc: https://github.com/nuxt-community/google-fonts-module
+    "@nuxtjs/google-fonts"
+  ],
+  googleFonts: {
+    Inter: true
+  }
+};
